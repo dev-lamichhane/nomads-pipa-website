6aedbcd2917a1879b07c296c7d285ee6990233d4
Dave Lamichhane <deveshl@protonmail.com>
2025-03-03 20:45:28 -0300

    Fixed Sanity Studio route and added root layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { Inter, Fira_Code } from "next/font/google";
 import "../styles/globals.css";
+import { NextStudio } from "next-sanity/studio";  // Import NextStudio
+import config from "../../sanity.config";  // Import config
 
 const inter = Inter({
   subsets: ["latin"],
@@ -14,7 +16,11 @@ const firaCode = Fira_Code({
 function MyApp({ Component, pageProps }) {
   return (
     <div className={`${inter.variable} ${firaCode.variable} antialiased`}>
-      <Component {...pageProps} />
+      {pageProps?.studio ? (
+        <NextStudio config={config} />  // Show Sanity Studio when studio prop exists
+      ) : (
+        <Component {...pageProps} />  // Render the page component for the rest of the app
+      )}
     </div>
   );
 }
