be153422f397dc483e7b188c3756ebbb86ba99e0
Dave Lamichhane <deveshl@protonmail.com>
2025-03-06 18:00:51 -0300

    Added Post Schema to Sanity

---
+++ src/sanity/schemaTypes/post.ts
@@ -0,0 +1,20 @@
+import { defineType } from 'sanity';
+
+export const post = defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'text',
+    },
+  ],
+});
+
